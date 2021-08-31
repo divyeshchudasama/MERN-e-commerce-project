530dafbcfe86f24a7da24409be2b319d2ed1df4c
Divyesh Chudasama <divyesh.chudasama22@gmail.com>
2021-08-31 16:26:08 +0530

    login & signup addded
    login & signup addded

--- e-mart/src/components/footer/Footer.jsx
+++ e-mart/src/components/footer/Footer.jsx
@@ -7,38 +7,59 @@ function Footer() {
     <>
       <footer className={`footer mt-auto  bg-default ${styles.footerBg}`}>
         <div className="container">
-          <div class="row">
-            <div class="col-lg-4 col-sm-6 col-12" >
-              <h4 className={`${styles.base1}`}>Companey</h4>
-              <p className={`text-muted ${styles.base2}`}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Ad quibusdam quis voluptatum, soluta harum id officiis dolore, aperiam architecto
+          <div className="row">
+            <div className="col-lg-4 col-sm-6 col-12">
+              <h4 className={`${styles.base1} text-white`}>Company</h4>
+              <p className={`text-muted ${styles.base2}`}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
+                quibusdam quis voluptatum, soluta harum id officiis dolore,
+                aperiam architecto
               </p>
             </div>
-            <div class="col-lg-4 col-sm-6 col-12" >
-              <h4 className={`${styles.base1}`}>NewsLetter</h4>
+            <div className="col-lg-4 col-sm-6 col-12">
+              <h4 className={`${styles.base1} text-white`}>NewsLetter</h4>
               <p className="text-muted">Join us for get latest update</p>
               <div className="d-flex">
-                <input type="name" placeholder="Enter a email" className="rounded-0 border-r-0" required></input>
+                <input
+                  type="name"
+                  placeholder="Enter an email"
+                  className="form-control form-control-sm me-2"
+                  style={{ width: "18em" }}
+                  required
+                ></input>
                 <div className="">
-                  <button type="submit" className="btn btn-success btn-sm">Subscribe</button>
+                  <button type="submit" className="btn btn-success btn-sm">
+                    Subscribe
+                  </button>
                 </div>
               </div>
             </div>
-            <div class="col-lg-4 col-sm-6 col-12" >
-              <h4 className={`${styles.base1}`}>Contact</h4>
+            <div className="col-lg-4 col-sm-6 col-12">
+              <h4 className={`${styles.base1} text-white`}>Contact</h4>
               <ul className={` ${styles.foot_ul}`}>
-                <li>🗺<span className="text-muted">E-mart store United state</span></li>
-                <li>📞<span className="text-muted">000-000-000</span></li>
-                <li>📠<span className="text-muted">123456</span></li>
-                <li>📧<span className="text-muted">e-mart@gmail.com</span></li>
+                <li>
+                  🗺<span className="text-muted">E-mart store United state</span>
+                </li>
+                <li>
+                  📞<span className="text-muted">000-000-000</span>
+                </li>
+                <li>
+                  📠<span className="text-muted">123456</span>
+                </li>
+                <li>
+                  📧<span className="text-muted">e-mart@gmail.com</span>
+                </li>
               </ul>
-
             </div>
           </div>
-
         </div>
         <div className={`${styles.bottfoot}`}>
-          <p className="text-center mb-0">Copyright @2021 Restant . All Rights Reserved By <a href="" className="font-weight-bold text-dark">E-mart</a></p>
+          <p className="text-center mb-0">
+            Copyright @2021 Restant . All Rights Reserved By{" "}
+            <a href="/" className="font-weight-bold text-light">
+              E-mart
+            </a>
+          </p>
         </div>
       </footer>
     </>
--- e-mart/src/components/header/Header.jsx
+++ e-mart/src/components/header/Header.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
 function Header() {
   return (
     <>
-      <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+      <Navbar
+        className={styles.headerBg}
+        bg="default"
+        variant="dark"
+        expand="lg"
+        sticky="top"
+      >
         <Container>
           <Link className="navbar-brand fw-bold" to="/">
             E-MART
@@ -14,8 +22,11 @@ function Header() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
+              <Link className="nav-link text-white" to="/">
+                SHOP
+              </Link>
               <Link className="nav-link text-white" to="/cart">
-                CART 🛒
+                CART <FontAwesomeIcon icon={faShoppingCart} />
               </Link>
               <Link className="nav-link text-white" to="/login">
                 LOGIN
