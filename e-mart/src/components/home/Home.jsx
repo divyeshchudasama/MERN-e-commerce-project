07f63aaed71f88c05c4f6c6f13e1b50843eab8aa
praveenpadmashali <34094504+praveenpadmashali@users.noreply.github.com>
2021-09-01 11:16:50 +0530

    static home screen
    static home screen

--- e-mart/src/components/home/Home.jsx
+++ e-mart/src/components/home/Home.jsx
@@ -1,12 +1,238 @@
 import React from "react";
+import { CardGroup, Card, Button } from "react-bootstrap";
 import styles from "./Home.module.css";
 
 function Home() {
   return (
     <div>
-      <h1>Home works</h1>
-      {/* <p>gayithri</p> */}
-      <button className="btn btn-success">Test button</button>
+      <CardGroup className="mt-5">
+        <Card>
+          <Card.Img
+            variant="top"
+            className={styles.imgheight}
+            src="https://res.cloudinary.com/devatchannel/image/upload/v1598287781/ecommerce/t5ycokodqfoe9dkqgaf7.jpg"
+          />
+          <Card.Body>
+            <Card.Title>Man Glass</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <div class="row_btn">
+              <a id="btn_buy">
+                <Button variant="success">Add to Cart</Button>{" "}
+              </a>
+              <a id="btn_view">
+                <Button variant="dark">View Item</Button>{" "}
+              </a>
+            </div>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img
+            variant="top"
+            className={styles.imgheight}
+            src="https://res.cloudinary.com/devatchannel/image/upload/v1598287748/ecommerce/ds52cjkcr4t0fvrnt6y6.jpg"
+          />
+          <Card.Body>
+            <Card.Title>Man Vest</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.{" "}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <div class="row_btn">
+              <a id="btn_buy">
+                <Button variant="success">Add to  Cart</Button>{" "}
+              </a>
+              <a id="btn_view">
+                <Button variant="dark">View Item</Button>{" "}
+              </a>
+            </div>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img
+            variant="top"
+            className={styles.imgheight}
+            src="https://res.cloudinary.com/devatchannel/image/upload/v1598287704/ecommerce/kqu2ktbrfmb5cjqbom2n.jpg"
+          />
+          <Card.Body>
+            <Card.Title>Man Cool</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <div class="row_btn">
+              <a id="btn_buy">
+                <Button variant="success">Add to Cart</Button>{" "}
+              </a>
+              <a id="btn_view">
+                <Button variant="dark">View Item</Button>{" "}
+              </a>
+            </div>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+      <CardGroup className="mt-5">
+        <Card>
+          <Card.Img
+            variant="top"
+            className={styles.imgheight}
+            src="https://res.cloudinary.com/devatchannel/image/upload/v1598287781/ecommerce/t5ycokodqfoe9dkqgaf7.jpg"
+          />
+          <Card.Body>
+            <Card.Title>Man Glass</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <div class="row_btn">
+              <a id="btn_buy">
+                <Button variant="success">Add to Cart</Button>{" "}
+              </a>
+              <a id="btn_view">
+                <Button variant="dark">View Item</Button>{" "}
+              </a>
+            </div>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img
+            variant="top"
+            className={styles.imgheight}
+            src="https://res.cloudinary.com/devatchannel/image/upload/v1598287748/ecommerce/ds52cjkcr4t0fvrnt6y6.jpg"
+          />
+          <Card.Body>
+            <Card.Title>Man Vest</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.{" "}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <div class="row_btn">
+              <a id="btn_buy">
+                <Button variant="success">Add to Cart</Button>{" "}
+              </a>
+              <a id="btn_view">
+                <Button variant="dark">View Item</Button>{" "}
+              </a>
+            </div>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img
+            variant="top"
+            className={styles.imgheight}
+            src="https://res.cloudinary.com/devatchannel/image/upload/v1598287704/ecommerce/kqu2ktbrfmb5cjqbom2n.jpg"
+          />
+          <Card.Body>
+            <Card.Title>Man Cool</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <div class="row_btn">
+              <a id="btn_buy">
+                <Button variant="success">Add to Cart</Button>{" "}
+              </a>
+              <a id="btn_view">
+                <Button variant="dark">View Item</Button>{" "}
+              </a>
+            </div>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+      <CardGroup className="mt-5">
+        <Card>
+          <Card.Img
+            variant="top"
+            className={styles.imgheight}
+            src="https://res.cloudinary.com/devatchannel/image/upload/v1598287781/ecommerce/t5ycokodqfoe9dkqgaf7.jpg"
+          />
+          <Card.Body>
+            <Card.Title>Man Glass</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <div class="row_btn">
+              <a id="btn_buy">
+                <Button variant="success">Add to Cart</Button>{" "}
+              </a>
+              <a id="btn_view">
+                <Button variant="dark">View Item</Button>{" "}
+              </a>
+            </div>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img
+            variant="top"
+            className={styles.imgheight}
+            src="https://res.cloudinary.com/devatchannel/image/upload/v1598287748/ecommerce/ds52cjkcr4t0fvrnt6y6.jpg"
+          />
+          <Card.Body>
+            <Card.Title>Man Vest</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.{" "}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <div class="row_btn">
+              <a id="btn_buy">
+                <Button variant="success">Add to Cart</Button>{" "}
+              </a>
+              <a id="btn_view">
+                <Button variant="dark">View Item</Button>{" "}
+              </a>
+            </div>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img
+            variant="top"
+            className={styles.imgheight}
+            src="https://res.cloudinary.com/devatchannel/image/upload/v1598287704/ecommerce/kqu2ktbrfmb5cjqbom2n.jpg"
+          />
+          <Card.Body>
+            <Card.Title>Man Cool</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <div class="row_btn">
+              <a id="btn_buy">
+                <Button variant="success">Add to Cart</Button>{" "}
+              </a>
+              <a id="btn_view">
+                <Button variant="dark">View Item</Button>{" "}
+              </a>
+            </div>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
     </div>
   );
 }
