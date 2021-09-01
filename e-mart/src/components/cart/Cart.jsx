ffe5785fbbfd371993175a5906ae5a5218576abf
Gayithri K <Gayithri.K@BRILLIO.COM>
2021-09-01 16:28:12 +0530

    cart page & detail page
    cart page & detail page

--- e-mart/src/components/cart/Cart.jsx
+++ e-mart/src/components/cart/Cart.jsx
@@ -1,11 +1,96 @@
-import React from "react";
+import React, {useState}from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 import styles from "./Cart.module.css";
+import images from "../../images/bird.jpg"
+
 
 function Cart() {
+
+ const [count, setCount] = useState(0)
+  const decrementValue = () => {
+      setCount(count-1)
+  }
+
+  const incrementValue = () => {
+    setCount(count+1)
+   }
   return (
-    <div>
-      <h3>Cart</h3>
-    </div>
+    <section>
+      <div className="container py-5">
+        <div className="">
+          <div className="row">
+            <div className="col-lg-8 col-sm-8 col-12">
+              <div className="table-responsive">
+                <table className="table">
+                  <thead>
+                    <tr className={styles.cartBg}>
+                      <th className="text-white p-3">Product</th>
+                      <th className="text-white p-3">Quantity</th>
+                      <th className="text-white p-3">Price</th>
+                      <th className="text-white p-3">Subtotal</th>
+                    </tr>
+                  </thead>
+                  <tbody className="">
+                    <tr>
+                      <td>
+                          <div className="">
+                            {/* <p className="">LENOVO G50</p> */}
+                            <Link className={styles.links} to="/details">
+                             LENONO G50
+                             </Link>
+                          </div>
+                      </td>
+                      <td>
+                        <div className="mc-count">
+                          <input type="button" onClick="decrementValue()" value="-" className={styles.inc} onClick={decrementValue}/>
+                          <input type="text" name="quantity" value={count} maxLength="2" max="100" size="1" id="number" className={styles.num}/>
+                          <input type="button" onClick="incrementValue()" value="+" className={styles.inc} onClick={incrementValue}/>
+                        </div>
+                      </td>
+                      <td className="text-muted">Rs.500</td>
+                      <td className="font-width-bold">Rs. 555/-  <FontAwesomeIcon icon={faTrash} /></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div className="col-lg-4 col-sm-4 col-12">
+              <div className="">
+                <div className={styles.cartBg}>
+                  <h5 className={`text-white py-3 text-center mb-0`}>Your Order <span className={styles.detf}>(Details)</span></h5>
+                </div>
+                <div className="border py-3 px-3">
+                  <h5 className={`${styles.d} font-weight-bold`}>Billing Details</h5>
+                  <div className="d-flex justify-content-between">
+                    <div className="text-muted">Items Total</div>
+                    <div className="text-muted"> Rs. 500/-</div>
+                  </div>
+                  <div className="d-flex justify-content-between">
+                    <div className="text-muted">Delevery Fee</div>
+                    <div className="text-muted"> Rs. 00/-</div>
+                  </div>
+                  <div className="d-flex justify-content-between">
+                    <div className="text-muted">Tax & clear</div>
+                    <div className="text-muted"> Rs. 00/-</div>
+                  </div>
+                  <hr></hr>
+                  <div className="d-flex justify-content-between">
+                    <div className="text-muted"><h6>Total</h6></div>
+                    <div className="text-muted"><h6>Rs.500/-</h6></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+         
+        </div>
+
+      </div>
+    </section>
+
   );
 }
 
--- e-mart/src/components/details/Details.jsx
+++ e-mart/src/components/details/Details.jsx
@@ -1,12 +1,73 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen, faStar } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Details.module.css";
+import images from "../../images/bird.jpg"
 
 function Details() {
   return (
-    <div>
-      <h3>Details</h3>
-      <h2>Praveen</h2>
-    </div>
+    <section>
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-lg-5 col-sm-5 col-12">
+            <div >
+              <img  src={images} alt="" className={styles.dimage}></img>
+            </div>
+          </div>
+          <div className="col-lg-7 col-sm-7 col-12">
+            <div className="">
+              <h4>Accuseantium Doloremque</h4>
+            </div>
+
+            <div className="rating-wapper my-2">
+              <span className="">
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+              </span>
+              <a href="" className="m-3">1 reviews</a>
+            </div>
+            <div className="">
+              <ul className={styles.deus} >
+                <li><span className={styles.decs}>Manufacture</span><h6>DGP</h6></li>
+                <li><span className={styles.decs}>Product Code</span>Product123</li>
+                <li><span className={styles.decs}>Reward Points</span>300</li>
+                <li><span className={styles.decs}>Avaliabilty</span>In Stock</li>
+              </ul>
+            </div>
+            <div className="my-3">
+              <h4>Description</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque mollitia harum quae corrupti vero minus alias molestias
+                 non neque fuga doloribus doloremque voluptates iusto iure pariatur vitae, cum qui cumque?</p>
+            </div>
+            <div className={`d-flex justify-content-between align-item-center points`}>
+          
+                <div className="">
+                  <ul className="list-unstyled">
+                    <li><h3 className="">Rs.500/-</h3></li>
+                    <li className="">Ex Tax: Rs. 85.00</li>
+               
+                  </ul>
+                </div>
+                <div className="">
+                  <label>QTY</label>
+                  <input type="text" name="quality" value="1" size="2" id="" className={styles.qty}></input>
+                  <button variant="success"
+                        className={`${styles.btn}`}
+                        type="submit">Add To cart</button>
+                
+                </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+    // <div>
+    //   <h3>Details</h3>
+    //   <h2>Praveen</h2>
+    // </div>
   );
 }
 
